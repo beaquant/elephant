796464d81a6102e752edae30e7cd4676db2acbf1
Eric <hu_zhihao@sina.cn>
2017-04-08 02:24:07 -0700

    library test project

---
+++ Code/driver/libTest/src/Elephant.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Elephant.c
+ Author      : Eric
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("%d\n", pumperSetNumber(1));
+	return EXIT_SUCCESS;
+}
