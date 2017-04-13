d9509694624323113cd6f0d1a744839e53786096
Eric <hu_zhihao@sina.cn>
2017-04-13 08:06:52 -0700

    commit *.so and makefile

---
+++ Code/driver/clib/RASPI-DEBUG/src/Jconfig/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Jconfig/Jconfig.c 
+
+OBJS += \
+./src/Jconfig/Jconfig.o 
+
+C_DEPS += \
+./src/Jconfig/Jconfig.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/Jconfig/%.o: ../src/Jconfig/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-linux-gnueabihf-gcc-4.9 -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
