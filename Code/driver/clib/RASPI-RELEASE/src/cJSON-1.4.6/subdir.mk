e637eefd318a7b422dd4a33abfc170be0f54825c
Eric <hu_zhihao@sina.cn>
2017-04-13 09:03:04 -0700

    build for all configuration

---
+++ Code/driver/clib/RASPI-RELEASE/src/cJSON-1.4.6/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/cJSON-1.4.6/cJSON.c 
+
+OBJS += \
+./src/cJSON-1.4.6/cJSON.o 
+
+C_DEPS += \
+./src/cJSON-1.4.6/cJSON.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/cJSON-1.4.6/%.o: ../src/cJSON-1.4.6/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
--- Code/driver/clib/Release/src/subdir.mk
+++ Code/driver/clib/Release/src/subdir.mk
@@ -55,8 +55,8 @@ C_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cross GCC Compiler'
-	arm-linux-gnueabihf-gcc-4.9 -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
