7e5f824cd42d0242a3e7181e32278a6c52bb367e
Eric <hu_zhihao@sina.cn>
2017-04-24 08:29:19 -0700

    add QT project for boardTest

---
+++ Code/driver/boardTest/boardTest.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-03-24T06:24:16
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = boardTest
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+LIBS     += -L/home/eric/github/elephant/Code/driver/clib/Debug -lclib
+
+INCLUDEPATH += /home/eric/github/elephant/Code/driver/clib/src
---
+++ Code/driver/boardTest/mainwindow.cpp
@@ -0,0 +1,279 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "types.h"
+#include "74hc595.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_HC595PIN0_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT0_DAC714_CLR);
+}
+
+void MainWindow::on_HC595PIN0_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT0_DAC714_CLR);
+}
+
+void MainWindow::on_HC595PIN1_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT1_ADS7805_BYTE);
+}
+
+void MainWindow::on_HC595PIN1_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT1_ADS7805_BYTE);
+}
+
+void MainWindow::on_HC595PIN2_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT2_ADS7805_R_C);
+}
+
+void MainWindow::on_HC595PIN2_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT2_ADS7805_R_C);
+}
+
+void MainWindow::on_HC595PIN3_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT3_ADS7805_CS);
+}
+
+void MainWindow::on_HC595PIN3_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT3_ADS7805_CS);
+}
+
+void MainWindow::on_HC595PIN4_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT4_TLC5615_CS);
+}
+
+void MainWindow::on_HC595PIN4_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT4_TLC5615_CS);
+}
+
+void MainWindow::on_HC595PIN5_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT5_SN74F575_SNCLK);
+}
+
+void MainWindow::on_HC595PIN5_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT5_SN74F575_SNCLK);
+}
+void MainWindow::on_HC595PIN6_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT6_RESERVED_BUSY);
+}
+
+void MainWindow::on_HC595PIN6_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT6_RESERVED_BUSY);
+}
+void MainWindow::on_HC595PIN7_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT7_RESERVED);
+}
+
+void MainWindow::on_HC595PIN7_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT7_RESERVED);
+}
+void MainWindow::on_HC595PIN8_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT8_SN74F575_PTD0);
+}
+
+void MainWindow::on_HC595PIN8_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT8_SN74F575_PTD0);
+}
+void MainWindow::on_HC595PIN9_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT9_SN74F575_PTD1);
+}
+
+void MainWindow::on_HC595PIN9_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT9_SN74F575_PTD1);
+}
+void MainWindow::on_HC595PIN10_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT10_SN74F575_PTD2);
+}
+
+void MainWindow::on_HC595PIN10_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT10_SN74F575_PTD2);
+}
+
+void MainWindow::on_HC595PIN11_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT11_SN74F575_PTD3);
+}
+
+void MainWindow::on_HC595PIN11_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT11_SN74F575_PTD3);
+}
+void MainWindow::on_HC595PIN12_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT12_SN74F575_PTD4);
+}
+
+void MainWindow::on_HC595PIN12_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT12_SN74F575_PTD4);
+}
+
+void MainWindow::on_HC595PIN13_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT13_SN74F575_PTD5);
+}
+
+void MainWindow::on_HC595PIN13_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT13_SN74F575_PTD5);
+}
+
+void MainWindow::on_HC595PIN14_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT14_SN74F575_PTD6);
+}
+
+void MainWindow::on_HC595PIN14_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT14_SN74F575_PTD6);
+}
+
+void MainWindow::on_HC595PIN15_0_clicked()
+{
+    clrIoRefresh(IO_EX_595_BIT15_SN74F575_PTD7);
+}
+
+void MainWindow::on_HC595PIN15_1_clicked()
+{
+    setIoRefresh(IO_EX_595_BIT15_SN74F575_PTD7);
+}
+
+
+
+void MainWindow::on_Reset595_clicked()
+{
+    reset595Refresh();
+    ui->HC595PIN0_0->setChecked(true);
+    ui->HC595PIN1_0->setChecked(true);
+    ui->HC595PIN2_0->setChecked(true);
+    ui->HC595PIN3_0->setChecked(true);
+    ui->HC595PIN4_0->setChecked(true);
+    ui->HC595PIN5_0->setChecked(true);
+    ui->HC595PIN6_0->setChecked(true);
+    ui->HC595PIN7_0->setChecked(true);
+    ui->HC595PIN8_0->setChecked(true);
+    ui->HC595PIN9_0->setChecked(true);
+    ui->HC595PIN10_0->setChecked(true);
+    ui->HC595PIN11_0->setChecked(true);
+    ui->HC595PIN12_0->setChecked(true);
+    ui->HC595PIN13_0->setChecked(true);
+    ui->HC595PIN14_0->setChecked(true);
+    ui->HC595PIN15_0->setChecked(true);
+
+}
+/*
+void MainWindow::on_Get595_clicked()
+{
+    uint16_t dat = get595Buf();
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+    if(dat & 0x0001){
+        ui->HC595PIN0_1->setChecked(true);
+    }
+    else{
+        ui->HC595PIN0_0->setChecked(true);
+    }
+
+}
+*/
+
+void MainWindow::on_Get595_clicked()
+{
+
+}
